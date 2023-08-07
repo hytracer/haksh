43393072c1ff31c99c6a0ba8c4482ea79eae384b
hytracer <cin@moon.rip>
2023-08-08 01:21:44 +0200

    working thingy without the env yet

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,106 @@
 module Main where
 
 import System.IO
-import System.Process
+import System.Process (createProcess, waitForProcess, proc, runProcess, readProcess, shell, callCommand, StdStream(..), CreateProcess(..), CmdSpec(..))
 import System.Exit (ExitCode(..))
 
-import System.Directory (setCurrentDirectory, getCurrentDirectory)
+import System.Directory (setCurrentDirectory, getCurrentDirectory, doesFileExist, getHomeDirectory)
+
+import System.FilePath ((</>))
+
+import System.Environment (getEnv, setEnv, lookupEnv)
+
+import Control.Monad (when, void, liftM)
+
+import System.Exit (exitSuccess)
+
+import Data.Maybe (fromMaybe)
+
+import Control.Exception (catch, SomeException)
+
+import Data.List (isInfixOf, isPrefixOf, stripPrefix)
 
 data RedirectionType = InputRedirect | OutputRedirect
 
 main :: IO ()
 main = do
-  putStrLn "Welcome to haksh!"
+  putStrLn "Welcome to your shell!"
+  hakshrcExists <- doesFileExist ".hakshrc"
+  when hakshrcExists loadHakshrc
   loop
+
+loadHakshrc :: IO ()
+loadHakshrc = do
+  currentDirHakshrcExists <- doesFileExist ".hakshrc"
+  homeDir <- getHomeDirectory
+  homeDirHakshrcExists <- doesFileExist (homeDir </> ".hakshrc")
+
+  case (currentDirHakshrcExists, homeDirHakshrcExists) of
+    (True, _) -> processConfigFile ".hakshrc"
+    (_, True) -> processConfigFile (homeDir </> ".hakshrc")
+    _         -> putStrLn "No .hakshrc found."  -- No .hakshrc found
+
+processConfigFile :: FilePath -> IO ()
+processConfigFile configFile = do
+  hakshrcContents <- readFile configFile
+  mapM_ processConfigLine (lines hakshrcContents)
+
+processConfigLine :: String -> IO ()
+processConfigLine line
+  | isComment line = pure ()  -- Skip comment lines
+  | null (stripLeadingWhitespace line) = pure ()  -- Handle empty lines
+  | isAlias line   = processAlias line
+  | isEnvVar line  = processEnvVar line
+  | otherwise      = void $ executeExternalCommand (words line)
+
+isComment :: String -> Bool
+isComment line = "#" `isPrefixOf` stripLeadingWhitespace line
+
+isAlias :: String -> Bool
+isAlias line = "alias " `isPrefixOf` line
+
+isEnvVar :: String -> Bool
+isEnvVar line = "=" `isInfixOf` line
+
+stripLeadingWhitespace :: String -> String
+stripLeadingWhitespace = dropWhile (== ' ')
+
+processAlias :: String -> IO ()
+processAlias line = do
+  let (alias, cmd) = splitAlias line
+  when (not (null alias) && not (null cmd)) $ do
+    putStrLn $ "Adding alias: " ++ alias ++ " -> " ++ cmd
+    setAlias alias cmd
+
+splitAlias :: String -> (String, String)
+splitAlias line = case stripPrefix "alias " line of
+  Just rest -> span (/= ' ') rest
+  Nothing   -> ("", "")
+
+setAlias :: String -> String -> IO ()
+setAlias alias cmd = setEnv alias cmd
+
+
+getAlias :: String -> IO (Maybe String)
+getAlias alias = do
+  maybeValue <- lookupEnv alias
+  return maybeValue
+
+processEnvVar :: String -> IO ()
+processEnvVar line = do
+  let (var, value) = splitEnvVar line
+  when (not (null var) && not (null value)) $ do
+    putStrLn $ "Setting environment variable: " ++ var ++ "=" ++ value
+    setEnvVar var value
+
+splitEnvVar :: String -> (String, String)
+splitEnvVar line = case break (== '=') line of
+  (var, '=':value) -> (var, value)
+  _                -> ("", "")
+
+setEnvVar :: String -> String -> IO ()
+setEnvVar var value = setEnv var value
+
 
 loop :: IO ()
 loop = do
@@ -19,8 +108,8 @@ loop = do
   hFlush stdout
   input <- getLine
   -- TODO user input
-  if input == "exit"
-    then putStrLn "Goodbye"
+  if input == "test"
+    then putStrLn "wow"
     else do
       let commandTokens = words input
       exitCode <- executeCommand commandTokens
@@ -32,10 +121,37 @@ loop = do
 executeCommand :: [String] -> IO ExitCode
 executeCommand [] = pure ExitSuccess
 executeCommand (command:args)
+  | null command = pure ExitSuccess  -- Handle empty input
   | command == "cd"   = changeDirectory args
   | command == "pwd"  = printWorkingDirectory
   | command == "echo" = echoCommand args
-  | otherwise         = executeExternalCommand (command:args)
+  | otherwise         = do
+      aliasCommand <- getAlias command
+      case aliasCommand of
+        Just cmd -> do
+          putStrLn $ "Executing alias: " ++ cmd ++ " " ++ unwords args
+          (_, _, _, processHandle) <- createProcess (proc (head $ words cmd) (words $ unwords args)) {
+            cwd = Nothing,   -- Set the working directory to Nothing to use the current directory
+            std_out = Inherit,  -- Set stdout to inherit to print command output directly
+            std_err = Inherit,  -- Set stderr to inherit to print command errors directly
+            env = Nothing    -- Set the environment to Nothing to use the current environment
+          }
+          waitForProcess processHandle
+        Nothing  -> do
+          putStrLn $ "Executing command: " ++ command ++ " " ++ unwords args
+          (_, _, _, processHandle) <- createProcess (proc command args) {
+            cwd = Nothing,   -- Set the working directory to Nothing to use the current directory
+            std_out = Inherit,  -- Set stdout to inherit to print command output directly
+            std_err = Inherit,  -- Set stderr to inherit to print command errors directly
+            env = Nothing    -- Set the environment to Nothing to use the current environment
+          }
+          waitForProcess processHandle
+        `catch` handleCommandError
+
+handleCommandError :: SomeException -> IO ExitCode
+handleCommandError ex = do
+  putStrLn $ "Error executing command: " ++ show ex
+  pure $ ExitFailure 1
 
 printWorkingDirectory :: IO ExitCode
 printWorkingDirectory = do
@@ -43,9 +159,16 @@ printWorkingDirectory = do
   putStrLn cwd
   pure ExitSuccess
 
+expandEnvironmentVariable :: String -> IO String
+expandEnvironmentVariable ('$' : var) = do
+  value <- lookupEnv (tail var)
+  return $ fromMaybe ("$" ++ var) value
+expandEnvironmentVariable arg = return arg
+
 echoCommand :: [String] -> IO ExitCode
 echoCommand args = do
-  putStrLn (unwords args)
+  expandedArgs <- mapM expandEnvironmentVariable args
+  putStrLn (unwords expandedArgs)
   pure ExitSuccess
 
 changeDirectory :: [String] -> IO ExitCode
@@ -55,6 +178,7 @@ changeDirectory [] = do
 changeDirectory (dir:_) = do
   setCurrentDirectory dir
   pure ExitSuccess
+
 
 executeExternalCommand :: [String] -> IO ExitCode
 executeExternalCommand commandArgs = do
